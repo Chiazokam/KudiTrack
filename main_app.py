82185437e7c50d28ff154d95506a3e74af1f2138
Chiazokam <chiazokamecheta@gmail.com>
2018-09-21 21:54:05 +0100

    Update README, Connect flask script to MongoDB

---
+++ main_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import datetime
+import pymongo
+from pymongo import MongoClient # Database connector
+
+app = Flask(__name__)
+
+#==============================================================================
+#Create and connect to DB
+#==============================================================================
+client = MongoClient('localhost', 27017)    #Configure the connection to the database
+db = client.kuditracker    #Select the database
+
+#Insert dummy data
+expense = {
+           "date": datetime.datetime.now(),
+           "Description": "For house painting",
+           "amount": "N50000",
+           "category": "Personal"
+           }
+
+#expenses serves as the Collection
+db.expenses.insert_one(expense)
+
+#==============================================================================
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/business")
+def business():
+    return render_template("business.html")
+
+@app.route("/personal")
+def personal():
+    return render_template("personal.html")
+
+#==============================================================
+if __name__ == '__main__':
+    app.run(debug = True)
