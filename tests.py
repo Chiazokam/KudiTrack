4898a2e6de64206fa9748456c2a5a8bc8a80572c
Chiazokam <chiazokamecheta@gmail.com>
2018-10-30 15:18:19 +0100

    Write unit tests to check the routes

---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest
+from main_app import app
+
+
+class TestKudiApp(unittest.TestCase):
+    def test_rootPage(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_rootPageContent(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertTrue(b'Date: yyyy-mm-dd' in response.data)
+
+    def test_businessPage(self):
+        tester = app.test_client(self)
+        response = tester.get('/business', content_type='html/text')
+        self.assertTrue(b'All Business Expenses' in response.data)
+
+    def test_personalPage(self):
+        tester = app.test_client(self)
+        response = tester.get('/personal', content_type='html/text')
+        self.assertTrue(b'All Personal Expenses' in response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
