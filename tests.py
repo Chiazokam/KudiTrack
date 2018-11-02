90054482ad89dee61f89bcb620f469a858dbba32
chiazokam <chiazokamecheta@gmail.com>
2018-11-02 13:28:16 +0100

    --amend

--- tests.py
+++ tests.py
@@ -13,16 +13,5 @@ class TestKudiApp(unittest.TestCase):
         response = tester.get('/', content_type='html/text')
         self.assertTrue(b'Date: yyyy-mm-dd' in response.data)
 
-    def test_businessPage(self):
-        tester = app.test_client(self)
-        response = tester.get('/business', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    def test_personalPage(self):
-        tester = app.test_client(self)
-        response = tester.get('/personal', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-
 if __name__ == '__main__':
     unittest.main()
